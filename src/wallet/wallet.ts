c4b3cf7415db8ffd548e431c425994f0b17463d5
Weber❤️H2O2 <82763013+Weber-H2O2@users.noreply.github>
2021-12-25 15:48:52 +0800

    Searching as signers support

--- src/wallet/wallet.ts
+++ src/wallet/wallet.ts
@@ -4,6 +4,8 @@ import * as ecies from "../crypto/ecies";
 import * as db_wallet from "../model/database_wallet";
 import * as db_signer from "../model/database_signer";
 
+import { Op } from "sequelize";
+
 module.exports = function (app) {
   app.post("/user/:user_id/wallet", async function (req, res) {
     const user_id = req.params.user_id;
@@ -57,45 +59,36 @@ module.exports = function (app) {
     return;
   });
 
-  // app.get("/user/:user_id/as_signers", async function (req, res) {
-  //   const user_id = req.params.user_id;
-  //   if (!util.check_user_id(req, user_id)) {
-  //     console.log("user_id does not match with decoded JWT");
-  //     res.json(
-  //       util.Err(
-  //         util.ErrCode.InvalidAuth,
-  //         "user_id does not match, you can't see any other people's information"
-  //       )
-  //     );
-  //     return;
-  //   }
+  app.get("/user/:user_id/as_signers", async function (req, res) {
+    const user_id = req.params.user_id;
+    if (!util.check_user_id(req, user_id)) {
+      console.log("user_id does not match with decoded JWT");
+      res.json(
+        util.Err(
+          util.ErrCode.InvalidAuth,
+          "user_id does not match, you can't see any other people's information"
+        )
+      );
+      return;
+    }
 
-  //   console.log(req.query);
+    console.log(req.query);
 
-  //   let addresses: any = await db_wallet.findAllAddresses(user_id);
+    let addresses: any = await db_wallet.findAllAddresses(user_id);
 
-  //   await db_signer.findAll(
-  //     {
-  //       attributes: ["updatedAt", "name", "address"],
-  //     }
+    let signers = await db_signer.findAll({
+      attributes: ["updatedAt", "name", "address", "status"],
+      where: {
+        address: {
+          [Op.in]: addresses,
+        },
+      },
+      raw: true,
+    });
 
-  //   )
+    console.log(signers);
 
-  //   const findUsersInformation = function (ids) {
-  //     return userdb.findAll({
-  //       attributes: ["user_id", "email", "name"],
-  //       where: {
-  //         user_id: {
-  //           [Op.in]: ids,
-  //         },
-  //       },
-  //       raw: true,
-  //     });
-  //   };
-
-  //   console.log(addresses);
-
-  //   res.json(util.Succ(wallets));
-  //   return;
-  // });
+    res.json(util.Succ(signers));
+    return;
+  });
 };
