0f4c9fb914eb9cba9bec93b32b24681d0bcb06f3
Weber❤️H2O2@users <82763013+Weber-H2O2@users.noreply.github.com>
2022-01-09 17:29:09 +0800

    Draft implementation for registering public key srcipt

---
+++ utils/pk_register.ts
@@ -0,0 +1,68 @@
+const axios = require("axios");
+import querystring from "querystring";
+
+const relay_sdk = require("relay_sdk");
+let relayutil = relay_sdk.util;
+let relaysdk = relay_sdk.sdk;
+
+relayutil.require_env_variables([
+  "TEESDK_AUDITOR_BASE_DIR",
+  "TEESDK_AUDITOR_NAME",
+  "TEESDK_ENCLAVE_INFO_PATH",
+  "RELAY_ADDRESS",
+  "RELAY_PORT",
+]);
+
+const AUDITOR_BASE_DIR = process.env.TEESDK_AUDITOR_BASE_DIR;
+// auditor_name, e.g., "godzilla"
+const AUDITOR_NAME = process.env.TEESDK_AUDITOR_NAME;
+const ENCLAVE_INFO_PATH = process.env.TEESDK_ENCLAVE_INFO_PATH;
+const PUB = `${AUDITOR_BASE_DIR}/${AUDITOR_NAME}/${AUDITOR_NAME}.public.der`;
+const SIG = `${AUDITOR_BASE_DIR}/${AUDITOR_NAME}/${AUDITOR_NAME}.sign.sha256`;
+const ROOTCA = `deps/ias_root_ca_cert.pem`;
+
+const main = async () => {
+  const client = new relaysdk.EigenRelayClient(
+    "fns",
+    PUB,
+    SIG,
+    ROOTCA,
+    ENCLAVE_INFO_PATH,
+    process.env.RELAY_ADDRESS,
+    Number(process.env.RELAY_PORT)
+  );
+
+  try {
+    client.submit_task("EigenTEERegister", "", async (public_key) => {
+      console.log("Returns: ", public_key);
+
+      axios
+        .post(
+          "host.docker.internal:3000/store",
+          querystring.stringify({
+            digest: "1",
+            public_key: public_key,
+          }),
+          {
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }
+        )
+        .then((res) => res.data)
+        .catch((error) => {
+          console.error(`Failed to post public key`, error);
+          throw new Error(error.message);
+        });
+    });
+  } catch (e) {
+    console.log("Fail to submit task: ", e);
+  }
+};
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
