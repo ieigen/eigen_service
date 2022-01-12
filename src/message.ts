974bb055e31bf040d09921014d919d2cd9f7a354
eigmax <stephen@ieigen.com>
2022-01-13 00:45:16 +0800

    Fix protocol

--- src/message.ts
+++ src/message.ts
@@ -13,31 +13,22 @@ module.exports = function (app) {
 
         socket.on('confirmed', async (data) => {
             console.log(data)
+            // {network_id, user_id, token, confirmed_txlist}
+            // FIXME check token
             //update status
-            let confirmed_list: string[] = data.txhashlist;
+            let confirmed_list: string[] = data.confirmed_txlist;
             for (var txid in confirmed_list) {
                 await txh.updateOrAdd(txid, {status: txh.TransactionStatus.Success})
             }
 
             // get unconfirmed tx list
+            // FIXME query per user_id
             let confirming_list = await txh.search({network_id: data.network_id, status: txh.TransactionStatus.Sent}, 0, 10, false);
             let txid_list: string[] = []
             for (let tx in confirming_list.transactions) {
                 txid_list.push(tx["txid"])
             }
-            socket.emit('confirming', { txhashlist: txid_list, id: socket.id });
-        });
-
-        socket.on('confirming', async (data) => {
-            console.log(data)
-
-            // get unconfirmed tx list
-            let confirming_list = await txh.search({network_id: data.network_id, status: txh.TransactionStatus.Sent}, 0, 10, false);
-            let txid_list: string[] = []
-            for (let tx in confirming_list.transactions) {
-                txid_list.push(tx["txid"])
-            }
-            socket.emit('confirming', { txhashlist: txid_list, id: socket.id });
+            socket.emit('confirming', { unconfirmed_txlist: txid_list, id: socket.id });
         });
     });
 }
