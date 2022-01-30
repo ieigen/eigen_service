7631e3d637951590bf3bff6edadd27da75dbac8a
Weber❤️H2O2@users <82763013+Weber-H2O2@users.noreply.github.com>
2022-01-30 12:46:30 +0800

    Better check the order flag

--- src/model/database_transaction_history.ts
+++ src/model/database_transaction_history.ts
@@ -1,7 +1,7 @@
 import { Sequelize, Op, DataTypes, Order } from "sequelize";
 
 const getOrder = (order): Order => {
-  if (order) return [["updatedAt", "DESC"]];
+  if (order === 1) return [["updatedAt", "DESC"]];
   return [];
 };
 
