14de7b74acd5cab83402c5436a555628d634bef7
Weber❤️H2O2 <82763013+Weber-H2O2@users.noreply.github.com>
2022-06-26 18:18:50 +0800

    feat: Persistence session data

---
+++ src/model/database_session.ts
@@ -0,0 +1,121 @@
+// database_session.ts
+/**
+ * Session model definition
+ *
+ * @module database_session
+ */
+
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+import { Sequelize, DataTypes } from "sequelize";
+import consola from "consola";
+
+const sequelize = new Sequelize({
+  dialect: "sqlite",
+
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000,
+  },
+  storage: "./data/db_session.sqlite",
+});
+
+const sessiondb = sequelize.define("session_st", {
+  hash_code: {
+    type: DataTypes.CITEXT,
+    allowNull: false,
+    primaryKey: true,
+  },
+  token: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  expiry: {
+    type: DataTypes.NUMBER,
+    allowNull: false,
+  },
+  issue_time: {
+    type: DataTypes.NUMBER,
+    allowNull: false,
+  },
+});
+
+sequelize
+  .sync()
+  .then(function () {
+    return sessiondb.create({
+      user_id: 1,
+      network_id: "id",
+      user_address: "0xUSER",
+      cipher_key: "0x",
+    });
+  })
+  .then(function (row: any) {
+    consola.log(
+      row.get({
+        user_id: 1,
+        network_id: "id",
+        user_address: "0xUSER",
+      })
+    );
+    sessiondb.destroy({
+      where: {
+        user_id: row.user_id,
+        network_id: row.network_id,
+        user_address: row.user_address,
+      },
+    });
+  })
+  .catch(function (err) {
+    consola.log("Unable to connect to the database:", err);
+  });
+
+const add = function (hash_code, token, expiry, issue_time) {
+  return sessiondb.create({
+    hash_code,
+    token,
+    expiry,
+    issue_time,
+  });
+};
+
+const findOne = function (filter_dict) {
+  return sessiondb.findOne({ where: filter_dict });
+};
+const findAll = function (dict) {
+  return sessiondb.findAll({ where: dict });
+};
+
+const updateOrAdd = function (hash_code, token, expiry, issue_time) {
+  return sessiondb.findOne({ where: { hash_code } }).then(function (row: any) {
+    consola.log(row);
+    if (row === null) {
+      add(hash_code, token, expiry, issue_time);
+      return true;
+    }
+
+    return row
+      .update({
+        hash_code,
+        token,
+        expiry,
+        issue_time,
+      })
+      .then(function (result) {
+        consola.log("Update seesion success: " + result);
+        return true;
+      })
+      .catch(function (err) {
+        consola.log("Update seesion error: " + err);
+        return false;
+      });
+  });
+};
+
+const deleteSession = function (hash_code) {
+  return sessiondb.destroy({ where: { hash_code: hash_code } });
+};
+
+export { updateOrAdd, add, findOne, findAll, deleteSession };
--- src/session.ts
+++ src/session.ts
@@ -6,6 +6,7 @@
  */
 
 import consola from "consola";
+import * as db_session from "./model/database_session";
 
 /* eslint-disable @typescript-eslint/no-namespace */
 /* eslint-disable @typescript-eslint/prefer-namespace-keyword */
@@ -14,10 +15,14 @@ export module Session {
     token: string;
     expiry: number;
     issueTime: number;
-    constructor(t: string, e: number) {
+    constructor(t: string, e: number, issue_time?: number) {
       this.token = t;
       this.expiry = e;
-      this.issueTime = Math.floor(Date.now() / 1000);
+      if (issue_time) {
+        this.issueTime = issue_time;
+      } else {
+        this.issueTime = Math.floor(Date.now() / 1000);
+      }
     }
 
     isValid(this: session): boolean {
@@ -32,19 +37,36 @@ export module Session {
   }
 
   //TODO use redis in production
-  const user_token: Map<string, session> = new Map();
+  //     Now we use sequenize
+  // const user_token: Map<string, session> = new Map();
 
   export function check_token(key: string) {
-    const sess = user_token.get(key);
-    if (sess !== undefined && sess.isValid()) {
-      user_token.set(key, sess);
-      return sess.token;
-    }
-    user_token.delete(key);
-    return null;
+    return db_session.findOne({ hash_code: key }).then(function (row: any) {
+      if (row === null) {
+        // Not found the session
+        return null;
+      }
+
+      const token = row["dataValues"]["token"];
+      const expiry = row["dataValues"]["expiry"];
+      const issue_time = row["dataValues"]["issue_time"];
+
+      // NOTE: Shouls use issue_time in database here
+      const sess = new session(token, expiry, issue_time);
+
+      if (sess.isValid()) {
+        db_session.updateOrAdd(key, sess.token, sess.expiry, sess.issueTime);
+
+        return sess.token;
+      } else {
+        db_session.deleteSession(key);
+        return null;
+      }
+    });
   }
 
   export function add_token(key: string, sess: session) {
-    user_token.set(key, sess);
+    // user_token.set(key, sess);
+    return db_session.updateOrAdd(key, sess.token, sess.expiry, sess.issueTime);
   }
 }
